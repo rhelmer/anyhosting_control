f5ce5b6a2a2a4d9ea0a3a8c8597cdff85e561880
Robert Helmer <robert@roberthelmer.com>
2010-08-28 14:18:29 -0700

    working example

--- tools/anyhosting_adduser.sh
+++ tools/anyhosting_adduser.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash -x
 #
 # The rationale for doing it this way is explained in these blog posts:
 # 
@@ -19,15 +19,23 @@ useradd -d ${WEBDIR}/htdocs ${USERNAME}
 mkdir ${WEBDIR}
 chmod 701 ${WEBDIR}
 cd ${WEBDIR}
-mkdir htdocs logs conf tmp usr
+mkdir htdocs logs conf tmp usr etc lib bin var
 chmod 700 conf tmp
 chmod 701 htdocs
 chmod 750 logs
 chown www-data:${USERNAME} logs
 chown ${USERNAME}:${USERNAME} htdocs tmp
-echo "TODO Add to /etc/fstab and mount -a:"
-echo "/usr            ${WEBDIR}/usr ext3 bind,ro      0       0"
-echo "TODO Config copied into place, customize:"
+echo "/usr            ${WEBDIR}/usr ext3 bind,ro      0       0" > /etc/fstab
+echo "/etc            ${WEBDIR}/etc ext3 bind,ro      0       0" > /etc/fstab
+echo "/lib            ${WEBDIR}/lib ext3 bind,ro      0       0" > /etc/fstab
+echo "/bin            ${WEBDIR}/bin ext3 bind,ro      0       0" > /etc/fstab
+echo "/var            ${WEBDIR}/var ext3 bind,ro      0       0" > /etc/fstab
+mount -a
+echo "TODO Configs copied into place, customize:"
 cp /usr/local/etc/anyhosting_apache2.conf ${WEBDIR}/conf/apache2.conf
+# TODO automate
 echo "sensible-editor ${WEBDIR}/conf/apache2.conf"
-echo "TODO Create /etc/apache2/sites-enabled/${DOMAIN}"
+cp /usr/local/etc/anyhosting_apache_proxy.conf /etc/apache2/sites-available/${DOMAIN}
+# TODO automate
+echo "sensible-editor /etc/apache2/sites-available/${DOMAIN}
+a2ensite ${DOMAIN}
